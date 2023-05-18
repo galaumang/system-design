6449a316ad19e7279997422bcf882499b3706b93
Umang G <toumangg@gmail.com>
2023-05-18 17:46:51 +0530

    feat: machine code: snake ladder models add model classes for the snake and ladder solution implementation

---
+++ solutions-java/src/main/java/systemdesign/snakeandladder/models/Player.java
@@ -0,0 +1,14 @@
+package systemdesign.snakeandladder.models;
+
+public class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
